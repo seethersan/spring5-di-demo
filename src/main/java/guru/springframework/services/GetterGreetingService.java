87917aa77662446c84248f5609149c0a0207a3c9
Juan Carlos Espinoza Zurita <carlos_jcez@hotmail.com>
2019-12-30 13:50:56 -0500

    using spring Qualifier to identify the correct bean

---
+++ src/main/java/guru/springframework/services/GetterGreetingService.java
@@ -0,0 +1,11 @@
+package guru.springframework.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GetterGreetingService implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hello - i was injected by the getter";
+    }
+}
