f7831beda21c6120c568d413d18750d2e61abba0
Juan Carlos Espinoza Zurita <carlos_jcez@hotmail.com>
2019-12-31 18:29:31 -0500

    Dependency Injection errors fixed

--- src/main/java/guru/springframework/controllers/MyController.java
+++ src/main/java/guru/springframework/controllers/MyController.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class MyController {
-
+    
     private GreetingService greetingService;
 
     public MyController(GreetingService greetingService) {
--- src/main/java/guru/springframework/services/GreetingRepositoryImpl.java
+++ src/main/java/guru/springframework/services/GreetingRepositoryImpl.java
@@ -1,8 +1,11 @@
 package guru.springframework.services;
 
+import org.springframework.stereotype.Component;
+
 /**
  * Created by jt on 5/24/17.
  */
+@Component
 public class GreetingRepositoryImpl implements GreetingRepository {
 
     @Override
@@ -16,9 +19,5 @@ public class GreetingRepositoryImpl implements GreetingRepository {
     }
 
     @Override
-    public String getGermanGreeting() {
-
-        return "Primärer Grußdienst";
-
-    }
+    public String getGermanGreeting() { return "Primärer Grußdienst"; }
 }
